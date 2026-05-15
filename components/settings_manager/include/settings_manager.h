8ed948ac2fc99f7b75d12d3f3485725a0aabc278
OleksandrSapozhnik <creator.oleksandr@gmail.com>
2026-05-15 19:47:19 +0200

    feat(settings_manager): add NVS keys for local web client and static IP
    Add six new persisted settings: local_web_client_enabled (bool),
    sta_static_ip, sta_gateway, sta_netmask, mdns_hostname (strings),
    and ap_enabled (bool). Defaults: static IP fields empty,
    netmask 255.255.255.0, hostname "esp32-client", AP and LWC disabled.

--- components/settings_manager/include/settings_manager.h
+++ components/settings_manager/include/settings_manager.h
@@ -12,3 +12,17 @@ int         settings_manager_get_timeout(void);
 void settings_manager_save_location(const char* city);
 void settings_manager_save_price_zone(int index);
 void settings_manager_save_timeout(int index);
+
+bool        settings_manager_get_local_web_client_enabled(void);
+const char* settings_manager_get_sta_static_ip(void);
+const char* settings_manager_get_sta_gateway(void);
+const char* settings_manager_get_sta_netmask(void);
+const char* settings_manager_get_mdns_hostname(void);
+bool        settings_manager_get_ap_enabled(void);
+
+void settings_manager_save_local_web_client_enabled(bool enabled);
+void settings_manager_save_sta_static_ip(const char* ip);
+void settings_manager_save_sta_gateway(const char* gateway);
+void settings_manager_save_sta_netmask(const char* netmask);
+void settings_manager_save_mdns_hostname(const char* hostname);
+void settings_manager_save_ap_enabled(bool enabled);
