6eb18f17939055b1fbeefbfc8635ca299a2bf68f
TheFreeDuck <fredrikthorcewers@gmail.com>
2026-05-25 10:49:43 +0200

    Fix linuc stub settings mamanger

--- components/settings_manager/settings_manager_linux_stub.c
+++ components/settings_manager/settings_manager_linux_stub.c
@@ -3,6 +3,22 @@
 void settings_manager_init(void) {}
 const char* settings_manager_get_location(void) { return ""; }
 int settings_manager_get_price_zone(void) { return 0; }
+char* settings_manager_get_price_zone_as_string() {
+    int price_zone = settings_manager_get_price_zone();
+    if (price_zone == 0) {
+        return "SE1";
+    }
+    if (price_zone == 1) {
+        return "SE2";
+    }
+    if (price_zone == 2) {
+        return "SE3";
+    }
+    if (price_zone == 3) {
+        return "SE4";
+    }
+    return NULL;
+}
 int settings_manager_get_timeout(void) { return 0; }
 const char* settings_manager_get_ssid(void) { return ""; }
 const char* settings_manager_get_password(void) { return ""; }
